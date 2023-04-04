c84281069a4d2190011c9898f7b302904dec92b2
Melez <melez.maar@gmail.com>
2023-04-04 22:23:04 +0300

    Задания 19.1; 19.2 сделаны. Тесты проходят

--- exercises/19_concurrent_connections/task_19_1.py
+++ exercises/19_concurrent_connections/task_19_1.py
@@ -23,3 +23,48 @@
 а затем запустить эту функцию в разных потоках для разных
 IP-адресов с помощью concurrent.futures (это надо сделать в функции ping_ip_addresses).
 """
+import subprocess
+from concurrent.futures import ThreadPoolExecutor
+from pprint import pprint
+
+def ping_ip_address(ip_add):
+    command = subprocess.run(['ping', ip_add], stdout=subprocess.DEVNULL)
+    if command.returncode == 0:
+        return ip_add, True
+    else:
+        return ip_add, False
+
+def ping_ip_addresses(ip_list, limit=3): #limit
+    '''
+        Функция получает в качестве аргумента список ip адресов
+        она проверяет их  доступность и позвращает кортеж из двух списков
+        первый список доступных адресов, а втрой недоступных
+    '''
+    available_ip = [] # Доступные адреса
+    unavailable_ip = [] # недоступные адреса
+    with ThreadPoolExecutor(max_workers=limit) as executor:
+        future_list = []
+        for ip_address in ip_list:
+            future = executor.submit(ping_ip_address, ip_address)
+            future_list.append(future)
+        for f in future_list:
+            #print(f.result())
+            if f.result()[1] == True:
+                available_ip.append(f.result()[0])
+            else:
+                unavailable_ip.append(f.result()[0])
+        
+    result = (available_ip, unavailable_ip)
+    return result
+
+if __name__ == "__main__":
+    
+    list_of_ips = ["192.168.100.10", 
+               "8.8.8.8", 
+               "8.8.4.4",
+               "192.168.100.1", 
+               "192.168.100.4",
+               "192.168.100.2",
+               "192.168.100.3",]
+
+    print(ping_ip_addresses(list_of_ips, 5))
--- exercises/19_concurrent_connections/task_19_2.py
+++ exercises/19_concurrent_connections/task_19_2.py
@@ -34,3 +34,38 @@ Ethernet0/1                unassigned      YES NVRAM  administratively down down
 
 Проверить работу функции на устройствах из файла devices.yaml
 """
+
+from concurrent.futures import ThreadPoolExecutor
+import yaml
+from pprint import pprint
+from netmiko import (ConnectHandler)
+
+def send_show_command(device, command):
+    with ConnectHandler(**device) as ssh_session:
+        ssh_session.enable()
+        result = ssh_session.find_prompt() + command + '\n'
+        result += ssh_session.send_command(command) + '\n'
+        
+    return result
+
+def send_show_command_to_devices(devices, command, filename, limit=3):
+        #with open(devices) as f:
+            #devices_list = yaml.safe_load(f)
+        with ThreadPoolExecutor(max_workers=limit) as executor:
+            future_list = []
+            for device in devices:
+                future = executor.submit(send_show_command, device, command)
+                future_list.append(future)
+            result = [f.result() for f in future_list]    
+        with open (filename, 'w') as result_file:
+             result_file.writelines(result)    
+
+if __name__ == "__main__":
+    with open(r"C:\Users\user\Documents\Python\Training_Project\Training_clone_natenca\exercises\19_concurrent_connections\devices.yaml") as f:
+        devices_list = yaml.safe_load(f)
+    send_show_command_to_devices(devices_list,
+                                'sh ip int br', 
+                                r"C:\Users\user\Documents\Python\Training_Project\Training_clone_natenca\exercises\19_concurrent_connections\result.txt", 3)
+
+
+    
